08804ef697aec1e6dcd6600939aa89d6982eff73
Vishal Yadav <vishalyxo@gmail.com>
2024-04-07 22:38:38 +0530

    Fisrt

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+    <div className="bg-red-600">
+      Hello
+    </div>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-500 py-4">
+      <div className="container mx-auto bg-red-500 flex justify-between items-center">
+        <h1 className="text-white text-6xl font-bold">Expense Tracker</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="text-white hover:text-gray-200">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:text-gray-200">
+                About
+              </a>
+            </li>
+            {/* Add more navigation links as needed */}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
