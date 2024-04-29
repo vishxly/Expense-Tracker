538512f2a91b9a5cf5ffd96e818e85c7d9f83525
Vishal Yadav <vishalyxo@gmail.com>
2024-04-29 20:42:18 +0530

    final

--- src/services/expenseService.ts
+++ src/services/expenseService.ts
@@ -1,51 +1,51 @@
-import { config } from '../firebase/firebaseConfig';
-import { Expense } from '../components/types'; 
-import 'firebase/firestore'
+// import { config } from '../firebase/firebaseConfig';
+// import { Expense } from '../components/types'; 
+// import 'firebase/firestore'
 
-const db = config.firestore;
+// const db = config.firestore;
 
 
-const collectionName = 'expenses';
+// const collectionName = 'expenses';
 
-export const addExpense = async (expense: Expense) => {
-  try {
-    await db.collection(collectionName).add(expense);
-    console.log('Expense added successfully');
-  } catch (error) {
-    console.error('Error adding expense: ', error);
-  }
-};
+// export const addExpense = async (expense: Expense) => {
+//   try {
+//     await db.collection(collectionName).add(expense);
+//     console.log('Expense added successfully');
+//   } catch (error) {
+//     console.error('Error adding expense: ', error);
+//   }
+// };
 
-export const getExpenses = async (): Promise<Expense[]> => {
-  try {
-    const snapshot = await db.collection(collectionName).get();
-    const expenses: Expense[] = [];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    snapshot.forEach((doc: { data: () => any; id: any; }) => {
-      const data = doc.data();
-      expenses.push({ id: doc.id, ...data } as Expense);
-    });
-    return expenses;
-  } catch (error) {
-    console.error('Error getting expenses: ', error);
-    return [];
-  }
-};
+// export const getExpenses = async (): Promise<Expense[]> => {
+//   try {
+//     const snapshot = await db.collection(collectionName).get();
+//     const expenses: Expense[] = [];
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//     snapshot.forEach((doc: { data: () => any; id: any; }) => {
+//       const data = doc.data();
+//       expenses.push({ id: doc.id, ...data } as Expense);
+//     });
+//     return expenses;
+//   } catch (error) {
+//     console.error('Error getting expenses: ', error);
+//     return [];
+//   }
+// };
 
-export const deleteExpense = async (expenseId: string) => {
-  try {
-    await db.collection(collectionName).doc(expenseId).delete();
-    console.log('Expense deleted successfully');
-  } catch (error) {
-    console.error('Error deleting expense: ', error);
-  }
-};
+// export const deleteExpense = async (expenseId: string) => {
+//   try {
+//     await db.collection(collectionName).doc(expenseId).delete();
+//     console.log('Expense deleted successfully');
+//   } catch (error) {
+//     console.error('Error deleting expense: ', error);
+//   }
+// };
 
-export const updateExpense = async (expenseId: string, updatedExpense: Expense) => {
-  try {
-    await db.collection(collectionName).doc(expenseId).update(updatedExpense);
-    console.log('Expense updated successfully');
-  } catch (error) {
-    console.error('Error updating expense: ', error);
-  }
-};
+// export const updateExpense = async (expenseId: string, updatedExpense: Expense) => {
+//   try {
+//     await db.collection(collectionName).doc(expenseId).update(updatedExpense);
+//     console.log('Expense updated successfully');
+//   } catch (error) {
+//     console.error('Error updating expense: ', error);
+//   }
+// };
